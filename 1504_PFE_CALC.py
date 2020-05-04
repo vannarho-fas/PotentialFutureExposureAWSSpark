3a6d4b0abc9d0af492e8e926a33855b9ac5e3784
Forde Smith <forde.a.smith@gmail.com>
2020-05-04 13:10:03 +1000

    optimised map function, improved readability

--- 1504_PFE_CALC.py
+++ 1504_PFE_CALC.py
@@ -1,8 +1,10 @@
-# Update dates to current
+# A proof of concept that calculates potential future exposure
+# for a set of different OTC derivatives
+# using Quantlib, Spark and Amazon EMR
+
 
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.random import RandomRDDs
-from pyspark import AccumulatorParam
 import QuantLib as ql
 import datetime as dt
 import numpy as np
@@ -12,7 +14,7 @@ from boto3.session import Session
 
 
 # Used in loading the various input text files
-def is_number(s):
+def value_is_number(s):
     try:
         float(s)
         return True
@@ -21,29 +23,29 @@ def is_number(s):
 
 
 # QuantLib date to Python date
-def ql_to_datetime(d):
+def quantlib_date_to_datetime(d):
     return dt.datetime(d.year(), d.month(), d.dayOfMonth())
 
 
 # Python date to QuantLib date
-def py_to_qldate(d):
+def python_to_quantlib_date(d):
     return ql.Date(d.day, d.month, d.year)
 
 
 # Loads libor fixings from input file into an RDD and then collects the results
-def loadLiborFixings(libor_fixings_file):
+def load_libor_fixings(libor_fixings_file):
     libor_fixings = sc.textFile(libor_fixings_file) \
         .map(lambda line: line.split(",")) \
-        .filter(lambda r: is_number(r[1])) \
+        .filter(lambda r: value_is_number(r[1])) \
         .map(lambda line: (str(line[0]), float(line[1]))).cache()
 
-    fixingDates = libor_fixings.map(lambda r: r[0]).collect()
+    fixing_dates = libor_fixings.map(lambda r: r[0]).collect()
     fixings = libor_fixings.map(lambda r: r[1]).collect()
-    return fixingDates, fixings
+    return fixing_dates, fixings
 
 
 # Loads input swap specifications from input file into an RDD and then collects the results
-def load_swaps(instruments_file):
+def load_irs_swaps(instruments_file):
     swaps = sc.textFile(instruments_file) \
         .map(lambda line: line.split(",")) \
         .filter(lambda r: r[0] == 'IRS') \
@@ -63,7 +65,7 @@ def load_fxfwds(instruments_file):
 
 
 # Builds a QuantLib swap object from given specification
-def makeSwap(today, start, maturity, nominal, fixedRate, index, typ=ql.VanillaSwap.Payer):
+def create_quantlib_swap_object(today, start, maturity, nominal, fixedRate, index, typ=ql.VanillaSwap.Payer):
     calendar = ql.UnitedStates()
     fixedLegTenor = ql.Period(6, ql.Months)
     floatingLegBDC = ql.ModifiedFollowing
@@ -105,14 +107,12 @@ def main(sc, args_dict):
     # QuantLib SWIG wrappers require use of broadcast variables in Spark
 
     # Set up variables and data
-    session = Session(aws_access_key_id='AWS_ACCESS_KEY',
-                      aws_secret_access_key='AWS_SECRET_ACCESS_KEY')
+
     broadcast_dict = {}
     pytoday = dt.datetime(2020, 4, 7)
     broadcast_dict['python_today'] = pytoday
     today = ql.Date(pytoday.day, pytoday.month, pytoday.year)
     ql.Settings.instance().evaluationDate = today
-
     usd_calendar = ql.UnitedStates()
     usd_dc = ql.Actual365Fixed()
     eurusd_fx_spot = ql.SimpleQuote(1.1856)
@@ -124,7 +124,7 @@ def main(sc, args_dict):
     # Load EUR libor swap curve from input file
     eur_swap_curve = sc.textFile(args_dict['eur_swap_curve_file']) \
         .map(lambda line: line.split(",")) \
-        .filter(lambda r: is_number(r[1])) \
+        .filter(lambda r: value_is_number(r[1])) \
         .map(lambda line: (str(line[0]), float(line[1]))) \
         .cache()
     eur_curve_dates = eur_swap_curve.map(lambda r: r[0]).collect()
@@ -135,7 +135,7 @@ def main(sc, args_dict):
     # Loads USD libor swap curve from input file
     usd_swap_curve = sc.textFile(args_dict['usd_swap_curve_file']) \
         .map(lambda line: line.split(",")) \
-        .filter(lambda r: is_number(r[1])) \
+        .filter(lambda r: value_is_number(r[1])) \
         .map(lambda line: (str(line[0]), float(line[1]))) \
         .cache()
     usd_curve_dates = usd_swap_curve.map(lambda r: r[0]).collect()
@@ -150,28 +150,28 @@ def main(sc, args_dict):
     usdlibor3m = ql.USDLibor(ql.Period(3, ql.Months), usd_disc_term_structure)
 
     # don't need EUR fixings since we don't have a EUR swap
-    fixingDates, fixings = loadLiborFixings(libor_fixings_file)
-    fixingDates = [ql.DateParser.parseFormatted(r, '%Y-%m-%d') for r in fixingDates]
+    fixing_dates, fixings = load_libor_fixings(libor_fixings_file)
+    fixing_dates = [ql.DateParser.parseFormatted(r, '%Y-%m-%d') for r in fixing_dates]
     three_month_old_date = usd_calendar.advance(today, -90, ql.Days, ql.ModifiedFollowing)
-    latestFixingDates = fixingDates[fixingDates.index(three_month_old_date):]
-    latestFixings = fixings[fixingDates.index(three_month_old_date):]
-    usdlibor3m.addFixings(latestFixingDates, latestFixings)
-    broadcast_dict['fixing_dates'] = [ql_to_datetime(x) for x in latestFixingDates]
+    latestfixing_dates = fixing_dates[fixing_dates.index(three_month_old_date):]
+    latestFixings = fixings[fixing_dates.index(three_month_old_date):]
+    usdlibor3m.addFixings(latestfixing_dates, latestFixings)
+    broadcast_dict['fixing_dates'] = [quantlib_date_to_datetime(x) for x in latestfixing_dates]
     broadcast_dict['fixings'] = latestFixings
-    swaps = load_swaps(instruments_file)
+    swaps = load_irs_swaps(instruments_file)
     broadcast_dict['swaps'] = swaps
     fxfwds = load_fxfwds(instruments_file)
     broadcast_dict['fxfwds'] = fxfwds
 
     swaps = [
-        makeSwap(today, ql.DateParser.parseFormatted(swap[0], '%Y-%m-%d'),
-                 ql.Period(swap[1]), swap[2], swap[3], usdlibor3m,
-                 ql.VanillaSwap.Payer if swap[4] == 'Payer' else ql.VanillaSwap.Receiver)
+        create_quantlib_swap_object(today, ql.DateParser.parseFormatted(swap[0], '%Y-%m-%d'),
+                                    ql.Period(swap[1]), swap[2], swap[3], usdlibor3m,
+                                    ql.VanillaSwap.Payer if swap[4] == 'Payer' else ql.VanillaSwap.Receiver)
         for swap in swaps
     ]
 
     longest_swap_maturity = max([s[0].maturityDate() for s in swaps])
-    broadcast_dict['longest_swap_maturity'] = ql_to_datetime(longest_swap_maturity)
+    broadcast_dict['longest_swap_maturity'] = quantlib_date_to_datetime(longest_swap_maturity)
 
     Nsim = int(args_dict['NSim'])
     NPartitions = int(args_dict['NPartitions'])
@@ -188,7 +188,7 @@ def main(sc, args_dict):
     for idx in range(len(swaps)):
         dates += swaps[idx][1]
     dates = np.unique(np.sort(dates))
-    broadcast_dict['dates'] = [ql_to_datetime(x) for x in dates]
+    broadcast_dict['dates'] = [quantlib_date_to_datetime(x) for x in dates]
     br_dict = sc.broadcast(broadcast_dict)
 
     # Write out the time grid to a text file which can be parsed later
@@ -196,33 +196,34 @@ def main(sc, args_dict):
     temp_rdd = sc.parallelize(T)
 
     # coalesce with shrink the partition size to 1 so we have only 1 file to write and parse later
-    temp_rdd.coalesce(1).saveAsTextFile(output_dir + '/time-grid')
+    temp_rdd.coalesce(1).saveAsTextFile(output_dir + '/time_curve')
 
     # The main routine, generate a matrix of normally distributed random numbers and
     # spread them across the cluster
 
-    randArrayRDD = RandomRDDs.normalVectorRDD(sc, Nsim, len(T), seed=1)
+    random_array_rdd = RandomRDDs.normalVectorRDD(sc, Nsim, len(T), seed=1)
 
     # for each row of the matrix, which corresponds to one simulated path,
     # compute netting set NPV (collateralised and uncollateralised)
 
-    npv_list = randArrayRDD.map(lambda p: (calc_exposure(p, T, br_dict))).collect()
+    npv_list = random_array_rdd.map(lambda p: (calculate_potential_future_exposure(p, T, br_dict))).collect()
 
-    # convert list to native dataframe
+    # convert list to dataframe
 
     npv_dataframe = sc.parallelize(npv_list)
 
-    # write out the npv cube, remove '[' and ']' added by numpy array to ease parsing later
+    # write out the npv cube - next step beyond this is to clean up the raw data file,
+    # calculate the exposure then create the visualisations
 
-    npv_dataframe.coalesce(1).saveAsTextFile(output_dir + '/npv_cube')
+    npv_dataframe.coalesce(1).saveAsTextFile(output_dir + '/exposure_scenarios')
 
 
-def calc_exposure(rnumbers, time_grid, br_dict):
+def calculate_potential_future_exposure(random_numbers, time_grid, br_dict):
     # get hold of broadcasted dictionary and rebuild the curves, libor index, swaps and FxFwd
     # we need to do this since QuantLib SWIG objects cannot be passed around
 
     broadcast_dict = br_dict.value
-    today = py_to_qldate(broadcast_dict['python_today'])
+    today = python_to_quantlib_date(broadcast_dict['python_today'])
     ql.Settings.instance().setEvaluationDate(today)
     usd_calendar = ql.UnitedStates()
     usd_dc = ql.Actual365Fixed()
@@ -232,9 +233,9 @@ def calc_exposure(rnumbers, time_grid, br_dict):
     maturity = 10
     a = broadcast_dict['a']
     sigma = broadcast_dict['sigma']
-    dates = [py_to_qldate(x) for x in broadcast_dict['dates']]
-    longest_swap_maturity = py_to_qldate(broadcast_dict['longest_swap_maturity'])
-    fixing_dates = [py_to_qldate(x) for x in broadcast_dict['fixing_dates']]
+    dates = [python_to_quantlib_date(x) for x in broadcast_dict['dates']]
+    longest_swap_maturity = python_to_quantlib_date(broadcast_dict['longest_swap_maturity'])
+    fixing_dates = [python_to_quantlib_date(x) for x in broadcast_dict['fixing_dates']]
     fixings = broadcast_dict['fixings']
 
     usd_curve_dates = broadcast_dict['usd_curve_dates']
@@ -258,9 +259,9 @@ def calc_exposure(rnumbers, time_grid, br_dict):
     engine = ql.DiscountingSwapEngine(usd_disc_term_structure)
     swaps = broadcast_dict['swaps']
     swaps = [
-        makeSwap(today, ql.DateParser.parseFormatted(swap[0], '%Y-%m-%d'),
-                 ql.Period(swap[1]), swap[2], swap[3], usdlibor3m,
-                 ql.VanillaSwap.Payer if swap[4] == 'Payer' else ql.VanillaSwap.Receiver)
+        create_quantlib_swap_object(today, ql.DateParser.parseFormatted(swap[0], '%Y-%m-%d'),
+                                    ql.Period(swap[1]), swap[2], swap[3], usdlibor3m,
+                                    ql.VanillaSwap.Payer if swap[4] == 'Payer' else ql.VanillaSwap.Receiver)
         for swap in swaps
     ]
 
@@ -323,7 +324,7 @@ def calc_exposure(rnumbers, time_grid, br_dict):
     spotmat = np.zeros(shape=(len(time_grid)))
     spotmat[:] = eurusd_fx_spot
 
-    # CSA terms for Collateral movements
+    # Notional Credit Support Annex terms for collateral
     IA1 = 0;
     IA2 = 0;
     threshold1 = 100000.0;
@@ -333,8 +334,7 @@ def calc_exposure(rnumbers, time_grid, br_dict):
     rounding = 5000.0
     collateral_held = IA2 - IA1
 
-    # assume collateral posted last week was 100K, a reasonable number given the exposure was about
-    # 280K and 50,000 Threshold
+    # assume collateral posted last week was 100K
     collateral_posted = 100000.0
 
     # the main loop of NPV computations
@@ -344,7 +344,7 @@ def calc_exposure(rnumbers, time_grid, br_dict):
                np.exp(- a * (time_grid[iT] - time_grid[iT - 1]))
 
         var = 0.5 * sigma * sigma / a * (1 - np.exp(-2 * a * (time_grid[iT] - time_grid[iT - 1])))
-        rnew = mean + rnumbers[iT - 1] * np.sqrt(var)
+        rnew = mean + random_numbers[iT - 1] * np.sqrt(var)
         usd_rmat[iT] = rnew
         # USD discount factors as generated by HW model
         usd_disc_factors = [1.0] + [A(time_grid[iT], time_grid[iT] + k) *
@@ -356,7 +356,7 @@ def calc_exposure(rnumbers, time_grid, br_dict):
                np.exp(- a * (time_grid[iT] - time_grid[iT - 1]))
 
         var = 0.5 * sigma * sigma / a * (1 - np.exp(-2 * a * (time_grid[iT] - time_grid[iT - 1])))
-        rnew = mean + rnumbers[iT - 1] * np.sqrt(var)
+        rnew = mean + random_numbers[iT - 1] * np.sqrt(var)
         eur_rmat[iT] = rnew
 
         # EUR discount factors as generated by HW model
@@ -385,7 +385,7 @@ def calc_exposure(rnumbers, time_grid, br_dict):
         gk_process = ql.GarmanKohlagenProcess(ql.QuoteHandle(ql.SimpleQuote(eurusd_fx_spot)),
                                               usd_disc_term_structure, eur_disc_term_structure, flat_vol_hyts)
         dt = time_grid[iT] - time_grid[iT - 1]
-        spotmat[iT] = gk_process.evolve(time_grid[iT - 1], spotmat[iT - 1], dt, rnumbers[iT - 1])
+        spotmat[iT] = gk_process.evolve(time_grid[iT - 1], spotmat[iT - 1], dt, random_numbers[iT - 1])
         nettingset_npv = 0.
         for s in range(len(swaps)):
             if usdlibor3m.isValidFixingDate(dates[iT]):
