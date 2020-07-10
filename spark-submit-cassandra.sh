dfd883a2781b374984f66c434d9dabc0f16bb3e9
Forde Smith <forde.a.smith@gmail.com>
2020-07-10 15:05:18 +1000

    spark-submit including datastax package confs

--- spark-submit-cassandra.sh
+++ spark-submit-cassandra.sh
@@ -6,6 +6,7 @@ export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export LD_LIBRARY_PATH=/usr/lib/hadoop/lib/native
 export SPARK_HOME=/usr/lib/spark
+export $PFE_PATH=/Users/forsmith/Documents/PotentialFutureExposureAWSSpark
 
 # submit Spark job - adjust s3 bucket, filenames and scenario variables as needed
 
@@ -14,9 +15,9 @@ spark-submit \
 --packages com.datastax.spark:spark-cassandra-connector_2.11:2.4.3 \
 --conf spark.cassandra.connection.host=localhost \
 --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/pfe_scenarios.py 10 48 0.376739 0.0209835 \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/1504_USD_LIB_SWAP_CURVE.csv \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/1504_EUR_LIB_SWAP_CURVE.csv \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/1504_USD3MTD156N.csv \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/1504_INSTRUMENTS.csv \
-/Users/forsmith/Documents/PotentialFutureExposureAWSSpark/output090720
+$PFE_PATH/pfe_scenarios.py 10 48 0.376739 0.0209835 \
+$PFE_PATH/1504_USD_LIB_SWAP_CURVE.csv \
+$PFE_PATH/1504_EUR_LIB_SWAP_CURVE.csv \
+$PFE_PATH/1504_USD3MTD156N.csv \
+$PFE_PATH/1504_INSTRUMENTS.csv \
+$PFE_PATH/output090720
